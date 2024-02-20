875207f47f0e12d90273ba7a89106c8d5eb26c55
Musabbehzahab4 <musabbehzahab4@gmail.com>
2024-02-20 15:46:34 +0500

    remove route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/testing',[ProfileController::class,'asdf']);
 
 // Route::get('/dashboard', function () {
 //     return view('dashboard');
